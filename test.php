eeb840d11a52d5cddad9d3d9b1c091a672dcb218
Robin <robin.pajula@gmail.com>
2026-05-22 00:52:03 +0300

    test10

--- test.php
+++ test.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 try {
-    $conn = new PDO("mysql:host=localhost;dbname=ita25pajula_like_dislike", "ita25pajula_testi", "tAbasalu1212");
+    $conn = new PDO("mysql:host=localhost;dbname=ita25pajula_like_dislike.db", "ita25pajula_testi", "tAbasalu1212");
     echo "Ühendus õnnestus!";
 } catch (PDOException $e) {
     echo "Viga: " . $e->getMessage();
